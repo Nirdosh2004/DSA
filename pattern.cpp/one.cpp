1ac3949887f7eb374dbac9dd74cffa979116412c
Nirdosh Kushwaha <148872957+Nirdosh2004@users.noreply.github.com>
2024-01-07 06:14:59 +0000

    done

---
+++ pattern.cpp/one.cpp
@@ -0,0 +1,20 @@
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    for(int i=0 ;i<4;i++){ //for rows(horizontal lines)
+        for(int j=0;j<5;j++){  //for columns (vertical lines)
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+
+
+    return 0;
+}
