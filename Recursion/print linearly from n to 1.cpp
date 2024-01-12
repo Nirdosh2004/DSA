eda9056e3f446eec3ca92f8093b1bcd22c459e86
Nirdosh Kushwaha <148872957+Nirdosh2004@users.noreply.github.com>
2024-01-12 09:18:30 +0530

    Create print linearly from n to 1.cpp

---
+++ Recursion/print linearly from n to 1.cpp
@@ -0,0 +1,21 @@
+//print linearly from n to 1
+
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+void fn( int count , int n){
+    if(n==count-1)
+    return ;
+    cout<<n<<endl;
+    n--;
+    fn(count,n);
+}
+
+int main(){
+    int count = 1;
+    int n;
+    cout<<"Enter value for n : ";
+    cin>>n;
+    fn(count , n);
+}
