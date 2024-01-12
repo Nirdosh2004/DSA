22fa07457c2dce0b1789932daa1d3779aa78cbdb
Nirdosh Kushwaha <148872957+Nirdosh2004@users.noreply.github.com>
2024-01-12 09:59:30 +0530

    Update print sum of first n numbers.cpp

--- Recursion/print sum of first n numbers.cpp
+++ Recursion/print sum of first n numbers.cpp
@@ -4,6 +4,27 @@
 #include<vector>
 using namespace std;
 
+
+inline int sumOfNnumbers(int n ){
+  if(n==0)return 0;
+  return n + sumOfNnumbers(n-1);
+}
+
+int main(){
+   
+    int n;
+    cout<<"Enter value for n : ";
+    cin>>n;
+    cout<<sumOfNnumbers( n );
+}
+ //OR
+
+//print sum of first n numbers
+
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
  int sum = 0; //global declaration
 
 inline int sumOfNnumbers(int i ,int n ){
