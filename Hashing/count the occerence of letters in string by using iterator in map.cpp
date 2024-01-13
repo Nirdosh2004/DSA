dbd17d1d34d8082720e954dfdd978a13f3ce064f
Nirdosh Kushwaha <148872957+Nirdosh2004@users.noreply.github.com>
2024-01-13 11:29:41 +0530

    Create  count the occerence of letters in string by using iterator in map.cpp

---
+++ Hashing/count the occerence of letters in string by using iterator in map.cpp
@@ -0,0 +1,29 @@
+// count the occerence of letters in string by using iterator in map 
+
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+
+int main(){
+  string s;
+  cout<<"Enter word :";
+  cin>>s;
+  
+  
+        map<char,int> mpp; //first data Type is for which number and second is for frequency of the number 
+    for(int i=0;i<s.length();i++){
+        mpp[s[i]]++;
+    }
+    
+  
+    //using iterator
+    for(auto it : mpp){
+        cout<<it.first<<"  ->  "<<it.second<<endl;
+    }
+    
+  
+    
+    
+    return 0;
+}
